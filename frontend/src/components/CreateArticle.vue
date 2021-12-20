68d6d71b8fb03f3bd165efe4ef2fc6d9c0c534e6
Stanislav Shevchuk <xahter.2000@gmail.com>
2021-12-20 23:42:38 +0100

    Front: forms creating for articles v 0.1.7

--- frontend/src/components/CreateArticle.vue
+++ frontend/src/components/CreateArticle.vue
@@ -1,13 +1,68 @@
 <template>
-  <h1>Create article page</h1>
+  <div class="container mt-4">
+    <form @submit.prevent="insertArticle">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Please Enter Your Title"
+        v-model="title"
+      />
+      <br />
+
+      <textarea
+        rows="8"
+        placeholder="Please Enter Article Body"
+        class="form-control"
+        v-model="body"
+      >
+      </textarea>
+      <button class="btn btn-success mt-4">Publish Article</button>
+    </form>
+    <div
+      v-if="error"
+      class="alert alert-warning alert-dismissible fade show mt-5"
+      role="alert"
+    >
+      <strong>{{ error }}</strong>
+    </div>
+  </div>
 </template>
 
 <script>
+import { csrftoken } from "../csrf/csrf_token.js";
 export default {
-
-}
+  data() {
+    return {
+      title: null,
+      body: null,
+      error: null,
+    };
+  },
+  methods: {
+    insertArticle() {
+      if (!this.title || !this.body) {
+        this.error = "Please add all fields";
+      } else {
+        fetch("api/articles/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-CSRFTOKEN": csrftoken,
+          },
+          body: JSON.stringify({ title: this.title, body: this.body }),
+        })
+          .then((response) => response.json())
+          .then(() => {
+            this.$router.push({
+              name: "home",
+            });
+          })
+          .catch((error) => console.log(error));
+      }
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
