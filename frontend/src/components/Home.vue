e3546ee2dc49c9a140e71a74f915f71eddb4a64b
Stanislav Shevchuk <xahter.2000@gmail.com>
2021-12-20 22:41:01 +0100

    Frontend: add browsable articles. v 0.1.5

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,13 +1,48 @@
 <template>
-  <h1>This is home page</h1>
+  <div class="container mt-4">
+    <div v-for="article in articles" :key="article.pk">
+      <p class="mb-0">
+        Author:
+        <span class="badge bg-primary">
+          {{ article.author }}
+        </span>
+      </p>
+      <h3>{{ article.title }}</h3>
+      <hr>
+    </div>
+  </div>
 </template>
 
 <script>
+import { csrftoken } from "../csrf/csrf_token.js";
 export default {
-
-}
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  methods: {
+    getAllArticles() {
+      fetch("api/articles/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFTOKEN": csrftoken,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          this.articles.push(...data);
+          // console.log(data)
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+  created() {
+    this.getAllArticles();
+  },
+};
 </script>
 
 <style>
-
 </style>
