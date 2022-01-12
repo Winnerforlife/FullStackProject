2f901fba49c8f8b125221df4d0d0ebf588b1e58e
Stanislav Shevchuk <xahter.2000@gmail.com>
2022-01-12 18:26:56 +0100

    .

--- FullStackBlog/settings.py
+++ FullStackBlog/settings.py
@@ -124,7 +124,7 @@ LOGOUT_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static.json')
 
 STATIC_URL = '/static/'
 
