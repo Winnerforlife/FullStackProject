8ffdab9ed9a9be5fc0648e42f414462761f2d96b
Stanislav Shevchuk <xahter.2000@gmail.com>
2022-01-13 12:50:01 +0100

    edit vue.config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -22,8 +22,8 @@ module.exports = {
 
         config.devServer
             // the first 3 lines of the following code have been added to the configuration
-            .public('http://127.0.0.1:8080')    
-            .host('127.0.0.1')    
+            .public('https://fullstack-blog-ars.herokuapp.com/')    
+            .host('fullstack-blog-ars.herokuapp.com')    
             .port(8080)
             .hotOnly(true)
             .watchOptions({poll: 1000})
